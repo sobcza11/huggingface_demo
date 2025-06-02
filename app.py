2031c3e6984db410e2890b3b7dd291ec54d0e763
Rand Sobczak Jr. <rand.sobczak@gmail.com>
2025-06-02 10:34:13 -0400

    Test minimal Gradio app to trigger logs

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import gradio as gr
-from transformers import pipeline
 
-summarizer = pipeline("summarization")
+def greet(name):
+    return f"Hello, {name}!"
 
-def summarize(text):
-    return summarizer(text)[0]['summary_text']
-
-demo = gr.Interface(fn=summarize, inputs="textbox", outputs="textbox")
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 demo.launch()
